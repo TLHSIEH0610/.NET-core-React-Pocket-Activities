93d8cf474316769bb22d6c65fa9f9fab21f437e7
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-09-05 20:56:13 +1000

    seed data

--- API/Program.cs
+++ API/Program.cs
@@ -26,4 +26,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var service = scope.ServiceProvider;
+try{
+    var context = service.GetRequiredService<DataContext>();
+   await context.Database.MigrateAsync();
+   await Seed.SeedData(context);
+
+}catch(Exception e){
+    var logger = service.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "error occurred during migration");
+}
+
 app.Run();
