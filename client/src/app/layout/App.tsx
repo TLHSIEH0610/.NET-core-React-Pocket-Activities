0d92ec777e5c6598a1dad92c0b7a1eb51eb0a478
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-09-09 00:06:41 +1000

    update default qurty option

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,10 @@
-import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import NavBar from "./NavBar";
-import axios from "axios";
 import "./styles.css";
 import { Container } from "semantic-ui-react";
 function App() {
   const { pathname } = useLocation();
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((res) => {});
-  });
 
   return (
     <div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,14 @@ import { Provider } from "react-redux";
 import store from "./app/store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchInterval: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
