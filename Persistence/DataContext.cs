78e299c66cb5bc1ec0b6e505d1f5c3304805bc4d
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-09-12 20:47:39 +1000

    add identity framework

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,15 +1,16 @@
 
 using Microsoft.EntityFrameworkCore;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<Activity> Activities {get; set;}
+        public DbSet<Activity> Activities { get; set; }
     }
 }
