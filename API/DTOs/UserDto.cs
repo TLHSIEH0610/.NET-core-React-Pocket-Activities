a2aaecf3b5d625732d94694612a8172925f3f75f
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-09-14 20:33:35 +1000

    setup login Dto and controller

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+//properties that will sned back to client after logining in
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Image { get; set; }
+        public string Username { get; set; }
+    }
+}
